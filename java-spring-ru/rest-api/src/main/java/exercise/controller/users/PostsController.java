3cfa0ed08d72eb585119602cace346007ccd4210
AAvchinnikova <avchinnikova_a@mail.ru>
2024-08-10 10:57:20 +0200

    homework

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -2,7 +2,6 @@ package exercise.controller.users;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,22 +14,5 @@ import exercise.model.Post;
 import exercise.Data;
 
 // BEGIN
-@RestController
-@RequestMapping("/api")
-public class PostsController {
-    private List<Post> posts = Data.getPosts();
 
-    @GetMapping("users/{id}/posts")
-    public List<Post> index(@PathVariable String id) {
-        return posts.stream().filter(p -> p.getUserId() == Integer.parseInt(id))
-                .toList();
-    }
-    @PostMapping("users/{id}/posts")
-    @ResponseStatus(HttpStatus.CREATED)
-      public Post create(@PathVariable int id, @RequestBody Post post) {
-        post.setUserId(id);
-        posts.add(post);
-        return post;
-    }
-}
 // END
